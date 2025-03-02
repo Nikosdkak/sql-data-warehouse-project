8ee8014f70d5ec958ef0ebe4f30609ef8b8f3f76
Nikosdkak <nikosdkaklamanos@gmail.com>
2025-03-02 22:31:31 +0200

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -41,36 +41,39 @@ BEGIN
 	PRINT '>> Insering Data into: silver.crm_cust_info';
 
 
-	INSERT INTO silver.crm_cust_info(
-		cst_id,
-		cst_key,
-		cst_firstname,
-		cst_lastname,
-		cst_marital_status,
-		cst_gndr,
-		cst_create_date)
-
-	select
-	cst_id,
-	cst_key,
-	TRIM (cst_firstname) AS cst_first_name,
-	TRIM (cst_lastname) AS cst_last_name,
-	case when UPPER (TRIM(cst_material_status)) = 'S' THEN 'Single'
-		WHEN UPPER (TRIM(cst_material_status)) = 'M' THEN 'Married'
-		ELSE 'N/A'
-		END cst_material_status,
-	case when UPPER (TRIM(cst_gndr)) = 'F' THEN 'FEMALE'
-		WHEN UPPER (TRIM(cst_gndr)) = 'M' THEN 'MALE'
-		ELSE 'N/A'
-		END cst_gndr,
-	cst_create_date
-	from(
-	select
-	*,
-	row_number() over (partition by cst_id order by cst_create_date desc) as flag_last
-	from bronze.crm_cust_info
-	where cst_id is null
-	)t where flag_last = 1
+	INSERT INTO silver.crm_cust_info (
+			cst_id, 
+			cst_key, 
+			cst_firstname, 
+			cst_lastname, 
+			cst_marital_status, 
+			cst_gndr,
+			cst_create_date
+		)
+		SELECT
+			cst_id,
+			cst_key,
+			TRIM(cst_firstname) AS cst_firstname,
+			TRIM(cst_lastname) AS cst_lastname,
+			CASE 
+				WHEN UPPER(TRIM(cst_material_status)) = 'S' THEN 'Single'
+				WHEN UPPER(TRIM(cst_material_status)) = 'M' THEN 'Married'
+				ELSE 'n/a'
+			END AS cst_material_status, -- Normalize marital status values to readable format
+			CASE 
+				WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+				WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+				ELSE 'n/a'
+			END AS cst_gndr, -- Normalize gender values to readable format
+			cst_create_date
+		FROM (
+			SELECT
+				*,
+				ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
+			FROM bronze.crm_cust_info
+			WHERE cst_id IS NOT NULL
+		) t
+		WHERE flag_last = 1;
 	SET @end_time = GETDATE();
 		PRINT'>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + 'seconds';
 		PRINT '-----------------------------------';
